7693ac473a046b2da44615cfad4bb1bb1d5d8e81
unknown <skyzerzlol@gmail.com>
2019-09-19 17:18:23 +0300

    App finished (2 months)

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://brad:brad123@ds147213.mlab.com:47213/devconnector1998"
+  mongoURI: "mongodb://brad:brad123@ds147213.mlab.com:47213/devconnector1998",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const gravatar = require("gravatar");
+const passport = require("passport");
 
 const users = require("./router/api/users");
 const profile = require("./router/api/profile");
 const posts = require("./router/api/posts");
 
 const app = express();
+
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB config
 const db = require("./config/keys").mongoURI;
@@ -19,10 +26,14 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello"));
+//passport middleware
+app.use(passport.initialize());
+
+// passport config
+require("./config/passport")(passport);
 
 //routes
-app.use('/api/users', users);
+app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
